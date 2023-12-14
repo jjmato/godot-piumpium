fa3cf1d967ed7274751baaa740f779c3bb66695e
jjmato <jjmatotorres@gmail.com>
2023-12-14 21:36:02 +0100

    feat: score and highscore

--- menus/game_over.gd
+++ menus/game_over.gd
@@ -1,7 +1,22 @@
 extends Control
 
+@export var game_stats: GameStats
+
+@onready var score_value_label = %ScoreValueLabel
+@onready var high_score_value_label = %HighScoreValueLabel
+
+func _ready() -> void:
+	if game_stats.score > game_stats.highscore:
+		game_stats.highscore = game_stats.score
+	
+	score_value_label.text = str(game_stats.score)
+	high_score_value_label.text = str(game_stats.highscore)
+
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
+		game_stats.score = 0;
 		get_tree().change_scene_to_file("res://menus/menu.tscn")
 	elif  Input.is_action_just_pressed('ui_reset'):
+		game_stats.score = 0;
 		get_tree().change_scene_to_file("res://world.tscn")
+		
